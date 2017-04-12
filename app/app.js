8acb8bac8fb327c24a32f0f3c6880b761ce47658
Adrian Makowiecki <adrian.makowiecki@plte.pl>
2017-04-13 01:20:40 +0200

    FBConnector

--- app/app.js
+++ app/app.js
@@ -1,3 +1,64 @@
-$(document).ready(function(){
-    alert('e-handluj z tym');
+$(document).ready(function() {
+    if (document.title.indexOf("starting page") > 0) {
+        alert('e-handluj z tym');
+        initFbAndRedirect();
+    } else if (window.location.href.indexOf("main") > 0) {
+        fbInit(function() {
+            setUsernameAndPicture();
+        });
+    } else {
+        fbInit();
+    }
+
 });
+
+function initFbAndRedirect() {
+    fbInit(function() {
+        fbLoginStatus(function(status) {
+            if (status === 'connected') {
+                window.location.href = "/html/main.html";
+            } else {
+                window.location.href = "/html/sign-in.html";
+            }
+        });
+    });
+}
+
+function loginAndRedirect() {
+    fbLogin(function() {
+        window.location.href = "/html/main.html";
+    });
+}
+
+function setUsernameAndPicture() {
+    getName(function(name) {
+        $("#username").text(name);
+    });
+
+    getPictureUrl(function(url) {
+        $("#picture").html("<img src='"+url+"' />");
+    });
+}
+
+function showArtists() {
+    getArtists(100, function(artists){
+        alert("Banda pedałów których słuchasz: \n" + artists);
+    });
+}
+
+function showFriends() {
+    getFriends(function(friends) {
+        var friendsNames = [];
+        friends.forEach( function(element, index) {
+            friendsNames.push(element.name);
+        });
+            alert("Frajerzy których znasz korzystający z naszej zajebistej apki: \n" +friendsNames);
+    });
+}
+
+
+function logoutAndRedirect() {
+    fbLogout(function() {
+        window.location = "/html/sign-in.html";
+    });
+}
---
+++ app/fbConnector.js
@@ -0,0 +1,122 @@
+//inicjalizacja Facebook SDK, koniecznie jako pierwsza wywoływana funkcja w dokumencie
+function fbInit(callback) {
+    window.fbAsyncInit = function() {
+        FB.init({
+            appId: '798879713594636',
+            xfbml: true,
+            cookie: true,
+            session: true,
+            version: 'v2.8'
+        });
+
+        if (callback) { callback(); }
+        FB.AppEvents.logPageView();
+
+    };
+
+    (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) {
+            return;
+        }
+        js = d.createElement(s);
+        js.id = id;
+        js.src = "http://connect.facebook.net/en_US/sdk/debug.js"; //lub connect.facebook.net/en_US/sdk.js dla wersji produkcyjnej
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+}
+
+
+function fbLoginStatus(callback) { //w argumencie callbacka zwraca status zalogowania ('connected' jeśli zalogowany)
+    FB.getLoginStatus(function(response) {
+        callback(response.status);
+    });
+}
+
+function fbRefreshSession(callback) {
+    fbLoginStatus(function(status) {
+        if (status === 'connected') {
+            callback();
+        } else {
+            fbLogin(callback);
+        }
+    });
+}
+
+
+function fbLogin(callback) {
+    console.log("Logowanie do fb...")
+    FB.login(function(response) {
+        if (response.status === 'connected') {
+            console.log("Zalogowano.")
+            callback();
+        }
+    }, { scope: 'public_profile,user_likes,user_friends,user_actions.music,publish_actions' });
+}
+
+function fbLogout(callback) {
+    fbLoginStatus(function(status) {
+        if (status === 'connected') {
+            FB.logout(function(response) {
+                callback();
+            });
+        }
+
+    });
+}
+
+function getName(callback) { //w argumencie dla callbacka zwraca nazwę zalogowanego użytkownika
+    fbRefreshSession(function() {
+        FB.api(
+            '/me',
+            'GET', { "fields": "name" },
+            function(response) {
+                callback(response.name);
+            }
+        );
+    });
+
+}
+
+function getPictureUrl(callback) { //w argumencie dla callbacka zwraca url zdjęcia profilowego
+    fbRefreshSession(function() {
+        FB.api(
+            '/me/picture',
+            'GET', {},
+            function(response) {
+                callback(response.data.url);
+            }
+        );
+    });
+}
+
+
+function getFriends(callback) { //w argumencie dla callbacka zwraca listę znajomych którzy korzystają z tej aplikacji (w sensie zalogowali się kiedyś)
+    FB.api(                     //lista elementów {id, nazwa użytkownika}
+        '/me/friends',
+        'GET', {},
+        function(response) {
+            callback(response.data)
+        }
+    );
+}
+
+
+function getArtists(limit, callback) { //w argumencie dla callbacka zwraca listę muzyków
+    fbRefreshSession(function() {
+        FB.api(
+            '/me',
+            'GET', { "fields": "music.limit(" + limit + ")" }, //[limit] obiektów na stronę
+            function(response) {
+                var artists = [];
+                if (response.music) {
+                    response.music.data.forEach(function(element, index) {
+                        artists.push(element.name);
+                    });
+                }
+                callback(artists);
+            }
+        );
+    });
+
+}
