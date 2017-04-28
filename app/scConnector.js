7b57f5769c99f2115fff63c6d926c7a5262f8f85
Mateusz Chodkowski <chotkos@gmail.com>
2017-04-28 10:36:01 +0200

    scConnector changes

--- app/scConnector.js
+++ app/scConnector.js
@@ -1,32 +1,39 @@
 var CLIENT_ID = '342b8a7af638944906dcdb46f9d56d98';
 
 function scInit() {
-    SC.initialize({ client_id: CLIENT_ID });
+    SC.initialize({
+        client_id: CLIENT_ID
+    });
 }
 
 function getTrack(trackId, callback) {
-    var successHandler = function(response){
+    var successHandler = function (response) {
         callback(response[0]);
     }
-    
-    SC.get('/tracks', { ids: trackId }).then(successHandler);
+
+    SC.get('/tracks', {
+        ids: trackId
+    }).then(successHandler);
 }
 
 function getTracksOfArtist(limit, artist, callback) {
-    var successHandler = function(tracks) {
+    var successHandler = function (tracks) {
         callback(tracks);
     }
-    
-    SC.get('/tracks', { q: artist, limit: limit }).then(successHandler);
+
+    SC.get('/tracks', {
+        q: artist,
+        limit: limit
+    }).then(successHandler);
 }
 
 function generateSoundWrapper(track, callback) {
-    var successHandler = function(oEmbed) {
+    var successHandler = function (oEmbed) {
         callback(oEmbed);
     }
-    
-    SC.oEmbed(track.stream_url + '?client_id=' + CLIENT_ID, { 
-        max_height: 200, 
-        auto_play: false 
+
+    SC.oEmbed(track.stream_url + '?client_id=' + CLIENT_ID, {
+        max_height: 200,
+        auto_play: false
     }).then(successHandler);
 }
